291d7748a5bf66d5d7aa134bb36bc705b0fe4658
“[firstname <[gmugsk@gmail.com]>
2024-06-16 13:14:09 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+/*Question 23: Conditional Tests: Write a series of conditional tests.
+ Predict the results of each test.*/
+let car = 'MunnerCar';
+//test 1
+console.log("Is car === 'MunnerCar'? I predict True.");
+console.log(car === 'MunnerCar'); // True
+//test 2
+console.log("Is car == 'toyota'? I predict True.");
+console.log(car == 'toyota'); // False
+//test3
+console.log("Is car !== 'VolksWAgon'? I predict True.");
+console.log(car !== 'toyota'); // False
+//test4
+console.log("Is car === 'Mercedez'? I predict True.");
+console.log(car === 'toyota'); // False
+//test 5
+console.log("Is car == 'JetCar'? I predict True.");
+console.log(car == 'toyota'); // False
+//test 1
+console.log("Is car === 'MunnerCar'? I predict False.");
+console.log(car === 'MunnerCar'); // True
+//test 2
+console.log("Is car == 'toyota'? I predict False.");
+console.log(car == 'toyota'); // False
+//test3
+console.log("Is car !== 'VolksWAgon'? I predict False.");
+console.log(car !== 'toyota'); // False
+//test4
+console.log("Is car === 'Mercedez'? I predict False.");
+console.log(car === 'toyota'); // False
+//test 5
+console.log("Is car == 'JetCar'? I predict False.");
+console.log(car == 'toyota'); // False
+export {};
+//ceate at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
